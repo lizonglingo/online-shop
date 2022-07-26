4863d093780d5cc5154e328cd135f0d80be3557e
DKCBBB <40293922+lizonglin313@users.noreply.github.com>
2022-07-26 23:20:54 +0800

    feat: middleware

--- gin/middleware/main.go
+++ gin/middleware/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"time"
 )
 
@@ -10,8 +11,9 @@ func main() {
 	router := gin.New()
 	// 添加 logger 和 recover 中间件
 	// 这样添加的中间件是全局使用的中间件
- 	router.Use(gin.Recovery(), gin.Logger())
-	router.Use(MyLogger())	// 使用自定义中间件
+	router.Use(gin.Recovery(), gin.Logger())
+	router.Use(MyLogger())      // 使用自定义中间件
+	router.Use(TokenRequired()) // 使用验证中间件
 
 	// 这样添加的中间件只在接收 "/goods" 路由组的请求才会使用
 	//goodsGroup := router.Group("/goods")
@@ -26,8 +28,22 @@ func main() {
 	router.Run(":8085")
 }
 
-func AuthRequired(c *gin.Context)  {
-
+func TokenRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var token string
+		for k, v := range c.Request.Header {
+			if k == "X-Token" {
+				token = v[0]
+			}
+		}
+		if token != "112233" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"msg": "未登录",
+			})
+			c.Abort()	// 必须使用c.Abort()终止本次请求
+		}
+		c.Next()
+	}
 }
 
 // 自定义中间件
@@ -44,6 +60,6 @@ func MyLogger() gin.HandlerFunc {
 		fmt.Printf("耗时： %v\n", end)
 		// 在请求做完后可以做一些其他事情 比如记录状态信息
 		status := c.Writer.Status()
-		fmt.Println("状态：",status)
+		fmt.Println("状态：", status)
 	}
 }
